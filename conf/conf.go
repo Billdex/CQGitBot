875065d748221011744824562cb99e07c51febd9
luoxin <pc.luoxin@foxmail.com>
2020-03-02 17:04:43 +0800

    配置文件不存在时会自动生成conf.json模板

--- conf/conf.go
+++ conf/conf.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"os"
 )
 
 type config struct {
@@ -25,15 +26,40 @@ var Cfg config
 
 //Load configs from json file
 func LoadConfig(path string) error {
-	buf, err := ioutil.ReadFile(path)
-	if err != nil {
-		log.Println("load config ", path, " failed: ", err)
-		return err
-	}
-	err = json.Unmarshal(buf, &Cfg)
-	if err != nil {
-		log.Println("config unmarshal json failed: ", err)
-		return err
+	_, err := os.Lstat(path)
+	if os.IsNotExist(err) {
+		log.Println("配置文件不存在！")
+		var confFile = `{
+		  "port": 7920,
+		  "git": {
+			"uri": "/git",
+			"secret": ""
+		  },
+		  "qq": {
+			"uri": "/qq",
+			"cq_port": 5700,
+			"group_id": [
+			  ""
+			]
+		  }
+		}`
+		if ioutil.WriteFile(path, []byte(confFile), 0644) != nil {
+			log.Fatalln("创建配置文件失败！")
+			return err
+		} else {
+			log.Println("创建配置文件成功")
+		}
+	} else {
+		buf, err := ioutil.ReadFile(path)
+		if err != nil {
+			log.Println("load config ", path, " failed: ", err)
+			return err
+		}
+		err = json.Unmarshal(buf, &Cfg)
+		if err != nil {
+			log.Println("config unmarshal json failed: ", err)
+			return err
+		}
 	}
 	return nil
 }
